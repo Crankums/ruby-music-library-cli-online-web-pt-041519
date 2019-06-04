680b11d660914dde5c95138e0ffa7fea4064c6f3
Craig Donavin <crdonavin@gmail.com>
2019-06-04 20:13:33 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -48,7 +48,8 @@ class MusicLibraryController
     input = gets.strip
     if Genre.find_by_name(input) != nil
       genre = Genre.find_by_name(input)
-      
+      sorted_list = genre.songs.sort_by{|song| song.name}
+      sorted_list.each.with_index{|song, index| puts "#{index + 1}. #{song.name} - #{song.genre.name}"}
     end
   end
 end
