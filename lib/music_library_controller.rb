90c623e93fa2cefc43417da944d9cdee867cb423
Craig Donavin <crdonavin@gmail.com>
2019-06-04 21:40:32 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -16,11 +16,13 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    input = gets.strip until input == "exit"
+    input = gets.strip #until input == "exit"
     
     case input
-    when "list songs"
-      list_songs
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
     end
   end
 
