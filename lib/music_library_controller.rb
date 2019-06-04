4decabf4de3fc181c4ea463c09fd913fa8c40a36
Craig Donavin <crdonavin@gmail.com>
2019-06-04 20:01:22 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -43,7 +43,7 @@ class MusicLibraryController
     end  
   end
   
-  def list_songs_by_artist
+  def list_songs_by_genre
     puts "Please enter the name of a genre:"
     inputs = gets.strip
     if Genre.find_by_name(input) != nil
