b20de65a917b62b60b3bcb85e5f62af4f57db43e
Craig Donavin <crdonavin@gmail.com>
2019-06-04 21:42:30 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -25,6 +25,13 @@ class MusicLibraryController
         list_artists
       when "list genres"
         list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+        
     end
   end
 
